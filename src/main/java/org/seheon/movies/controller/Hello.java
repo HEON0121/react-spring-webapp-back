8149b297a0ac68920e193f6abd164a64da891449
seheon Kim <seheon.emma@gmail.com>
2023-01-24 17:39:05 +1000

    feat : first commit

---
+++ src/main/java/org/seheon/movies/controller/Hello.java
@@ -0,0 +1,12 @@
+package org.seheon.movies.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
